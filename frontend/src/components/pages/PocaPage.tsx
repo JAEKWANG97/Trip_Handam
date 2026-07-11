3f9ed33c916b20bb20a84f5f382c2a1f8fc424b8
JAEKWANG97 <marmong@naver.com>
2026-07-11 11:43:20 +0900

    chore: remove legacy frontend and execution assets

--- frontend/src/components/pages/PocaPage.tsx
+++
@@ -1,9 +0,0 @@
-export const PocaPage = () => {
-  return (
-    <>
-      <div className="mt-40">
-
-      </div>
-    </>
-  )
-}
--- frontend/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.json' {
-  const value: any;
-  export default value;
-}
-
-export interface Error {
-  message: string;
-  status: number;
-}
